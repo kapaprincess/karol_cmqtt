029c093275467aa4bf36ad719a968cb55d018251
kapaprincess <tareaskarolp@gmail.com>
2024-11-05 16:48:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,7 @@ import streamlit as st
 import json
 import platform
 
-import os
 
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-from PIL import Image
-
-import glob
-
-
-from gtts import gTTS
-from googletrans import Translator
 
 # Muestra la versión de Python junto con detalles adicionales
 st.write("Versión de Python:", platform.python_version())
@@ -85,47 +74,5 @@ else:
     st.write('')
 
 st.link_button("Voice Interface", "https://finalmultimodal.streamlit.app/")
-
-st.title("Voice Interface")
-
-st.write("Press the button and speak to control the lights (purple and green) and the door.")
-
-stt_button = Button(label=" Speak ", width=200)
-
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
-
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
-        client1.on_publish = on_publish                            
-        client1.connect(broker,port)  
-        message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
-        ret= client1.publish("kpv_ctrl", message)
-      
 
 
