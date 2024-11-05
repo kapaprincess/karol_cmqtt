f43268dc0604476ee68e0334cea6d9b782130325
kapaprincess <tareaskarolp@gmail.com>
2024-11-05 16:20:35 -0500

    Update voice.py

--- pages/voice.py
+++ pages/voice.py
@@ -1,1 +1,12 @@
-
+import os
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+import time
+import glob
+import paho.mqtt.client as paho
+import json
+from gtts import gTTS
+from googletrans import Translator
