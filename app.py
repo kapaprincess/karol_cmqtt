894912ab76d81835f293529170e1f857d75d6d9b
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 14:10:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import platform
 #voz
 import os
 
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
+#from bokeh.models.widgets import Button
+#from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 from PIL import Image
 
