50b577c1f43b805c7996e7fcda4bd6bb5964d12c
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 18:33:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -159,6 +159,6 @@ if result:
         st.write(result.get("GET_TEXT"))
         client1.on_publish = on_publish                            
         client1.connect(broker,port)  
-        message =json.dumps({"Act2":result.get("GET_TEXT").strip()})
+        message =json.dumps({"Act3":result.get("GET_TEXT").strip()})
         ret= client1.publish("kpv_ctrlVoice", message)
 
