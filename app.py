8adf6ce5ce865eec8d04883cd98f287e44e3c1ee
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 16:42:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ if st.button('Close'):
 else:
     st.write('')
 
-values = st.slider('Selecciona el rango de valores',0.0, 100.0)
+values = st.slider('Selecciona el rango de valores',0.0, 180.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
@@ -89,7 +89,7 @@ if st.button('Enviar valor analógico'):
 else:
     st.write('')
 
-st.link_button("Voice Interface", "https://finalmultimodal.streamlit.app/")
+# st.link_button("Voice Interface", "https://finalmultimodal.streamlit.app/")
 
 # Control por voz
 
@@ -120,7 +120,7 @@ st.write("Press the button and speak to open or close.")
 
 stt_button = Button(label=" Speak ", width=200)
 
-stt_button.js_on_event("button_click", CustomJS(code="""
+stt_button.js_on_event("button_click", CustomJS(code=
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
@@ -137,7 +137,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
         }
     }
     recognition.start();
-    """))
+    ))
 
 result = streamlit_bokeh_events(
     stt_button,
