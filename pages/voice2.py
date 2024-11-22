dcc9730f4e26a5cb2186909e0f97dbd92583ea21
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 17:00:00 -0500

    Update voice2.py

--- pages/voice2.py
+++ pages/voice2.py
@@ -13,11 +13,34 @@ from googletrans import Translator
 
 st.title("Voice Interface")
 
-st.write("Press the button and speak to control the lights (purple and green) and the door.")
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+#broker="157.230.214.127"
+#port=1883
+client1= paho.Client("GIT-HUB")
+client1.on_message = on_message
+
+
+
+st.title("Voice Control")
+# image = Image.open('room1.jpg')
+# st.image(image)
+# st.subheader("CONTROL WITH VOICE")
+
+
+st.write("Press the button and speak to open or close.")
 
 stt_button = Button(label=" Speak ", width=200)
 
-stt_button.js_on_event("button_click", CustomJS(code="""
+stt_button.js_on_event("button_click", CustomJS(code= """
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
@@ -34,7 +57,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
         }
     }
     recognition.start();
-    """))
+   """))
 
 result = streamlit_bokeh_events(
     stt_button,
@@ -51,4 +74,64 @@ if result:
         client1.connect(broker,port)  
         message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
         ret= client1.publish("kpv_ctrl", message)
-      
+
+
+st.subheader("CONTROL WITH BUTTONS")
+
+st.text("Purple Light")
+
+if st.button('PURPLE ON'):
+    act2="turn the yellow light on"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act2":act2})
+    ret= client1.publish("kpv_ctrl", message)
+ 
+    #client1.subscribe("Sensores")
+    
+    
+else:
+    st.write('')
+
+if st.button('PURPLE OFF'):
+    act2="turn the yellow light off"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act2":act2})
+    ret= client1.publish("kpv_ctrl", message)
+  
+    
+else:
+    st.write('')
+
+st.text("Green Light")
+
+if st.button('GREEN ON'):
+    act2="turn the blue light on"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act2":act2})
+    ret= client1.publish("kpv_ctrl", message)
+ 
+    #client1.subscribe("Sensores")
+    
+    
+else:
+    st.write('')
+
+if st.button('GREEN OFF'):
+    act2="turn the blue light off"
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act2":act2})
+    ret= client1.publish("kpv_ctrl", message)
+  
+    
+else:
+    st.write('')
+    
+
