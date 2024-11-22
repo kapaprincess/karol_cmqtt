37f5ea252115e165958066622390711900f3f274
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 14:18:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ import platform
 
 #voz
 
+import os
+
+#from bokeh.models.widgets import Button
+#from bokeh.models import CustomJS
+#from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+
+import glob
+
+
+from gtts import gTTS
+from googletrans import Translator
 
 
 
