8cbc868d68caf4d0d52b9937c82ce8f59a85f60e
kapaprincess <tareaskarolp@gmail.com>
2024-11-22 17:46:33 -0500

    Update voice2.py

--- pages/voice2.py
+++ pages/voice2.py
@@ -13,67 +13,6 @@ from googletrans import Translator
 
 st.title("Voice Interface")
 
-def on_publish(client,userdata,result):             #create function for callback
-    print("el dato ha sido publicado \n")
-    pass
-
-def on_message(client, userdata, message):
-    global message_received
-    time.sleep(2)
-    message_received=str(message.payload.decode("utf-8"))
-    st.write(message_received)
-
-broker="157.230.214.127"
-port=1883
-client1= paho.Client("GIT-HUB")
-client1.on_message = on_message
-
-
-
-st.title("Voice Control")
-# image = Image.open('room1.jpg')
-# st.image(image)
-# st.subheader("CONTROL WITH VOICE")
-
-
-st.write("Press the button and speak to open or close.")
-
-stt_button = Button(label=" Speak ", width=200)
-
-stt_button.js_on_event("button_click", CustomJS(code= """
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-   """))
-
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
-        client1.on_publish = on_publish                            
-        client1.connect(broker,port)  
-        message =json.dumps({"Act2":result.get("GET_TEXT").strip()})
-        ret= client1.publish("kpv_ctrl", message)
 
 
 st.subheader("CONTROL WITH BUTTONS")
